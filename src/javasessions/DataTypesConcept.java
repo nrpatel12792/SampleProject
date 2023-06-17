8cca76f853e7ee834347897ef181ddb55b92d4b2
nrpatel12792 <nrpatel12792@gmail.com>
2023-06-17 12:53:16 -0400

    first commit

---
+++ src/javasessions/DataTypesConcept.java
@@ -0,0 +1,16 @@
+package javasessions;
+
+/*
+ * 
+ */
+public class DataTypesConcept {
+	
+	public static void main(String args[]) {
+		
+		String s = new String("test");
+		System.out.println(s);
+		
+
+		
+	}
+}
