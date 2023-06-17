2a3b610cc76495ebd37b45626d546887a4ed47bf
nrpatel12792 <nrpatel12792@gmail.com>
2023-06-17 13:12:40 -0400

    third commit

--- src/javasessions/DataTypesConcept.java
+++ src/javasessions/DataTypesConcept.java
@@ -9,7 +9,9 @@ public class DataTypesConcept {
 		
 		String s = new String("test");
 		System.out.println(s);
-		System.out.println("Welcome to java ");
+		System.out.println("Welcome to java");
+		int a=5, b=10;
+		System.out.println("value of a and b:" + a + " " + b);
 
 		
 	}
